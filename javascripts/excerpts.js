687f3553c5dd5c0d7e709eb2b78d53d001661c4d
aylwin <amk_green@live.com>
2015-01-06 22:27:43 -0800

    Site updated at 2015-01-07 06:27:43 UTC

--- javascripts/excerpts.js
+++ javascripts/excerpts.js
@@ -1,9 +1,10 @@
-// function to set text in article elements to just fill each page and with page number in :after
+// function to set text in div elements to just fill each 'page' and with 'page' number in :after
 
 // note: there is NO case here when text is too short!!!
 
 function pageSet(pageht){
     var pages = 1, 
+        cnt = 0,
         pagesID = 0, 
         rawID = 'z1',
         pagetxt = 0,
@@ -14,6 +15,7 @@ function pageSet(pageht){
         textmove = 0, 
         textkeep = 0,
         targetOld = 0,
+        targetDivs = $('.excerpt-words').length + 1,
         targetNew = document.getElementById('z2');
 //making sure pagesID scrollHeight works.... is just number, no 'px'
 //    var testWhile = document.getElementById(rawID).scrollHeight; 
@@ -21,8 +23,18 @@ function pageSet(pageht){
 // set the class of text containers to window height    
     $( ".excerpt-words" ).css( "max-height", pageht + "px" );
     
-// continue until there is nothing in next target to move and next target exists
-while ( (targetNew.innerHTML !== '') && (targetNew) ) {
+// markdown won't apply paragraphs within html block elements, so need to wrap <p>s
+    for (cnt = 1; cnt < targetDivs; cnt++) {
+    pagesID = "z" + cnt; 
+        var textID = document.getElementById(pagesID);
+    pagetxt = textID.innerHTML;    
+    pagetxt = '<p>' + pagetxt + '</p>';
+    pagetxt = pagetxt.replace(/\r\n\r\n/g, "</p> <p>").replace(/\n\n/g, "</p> <p>");
+    document.getElementById(pagesID).innerHTML = pagetxt;
+}
+    
+// continue until there is nothing in next target to move
+while  (targetNew.innerHTML !== '') {
 // repeat while content height of contents of article element is greater than element
 
         pagesID = "#z" + pages; 
@@ -34,9 +46,11 @@ while ( (targetNew.innerHTML !== '') && (targetNew) ) {
         targetOld = document.getElementById(rawID);
         targetNew = document.getElementById(rawNextID);
     
+
+    
 // check to see if there are is an extra article element, make one if not       
     if ( !targetNew ) {
-         $( pagesID ).after( "<article class='excerpt-words' style='max-height;" + pageht + "px;' id='" + rawNextID + "'></article>");}  
+         $( pagesID ).after( "<div class='excerpt-words' id='" + rawNextID + "'></div>");}  
     
 // do the actual shifting of text
     while ( targetOld.scrollHeight > pageht ) {
---
+++ javascripts/excerpts2.js
@@ -0,0 +1,100 @@
+/* Redone:
+Jan 04/15
+*/
+
+function pageSet(pageht){
+    // eleminate old divs on page resize
+    var pages = 1, 
+        pagesID, 
+        rawID,
+        pagetxt,
+        textbreak,
+        textmove, 
+        textkeep,
+        textover,
+        pageDone = false,
+        createAfterID = '#ex-hidden',
+        cutdivs = document.getElementsByClassName('excerpt-words');
+        pageht = Math.round(0.9 * pageht);
+    // elminate div.excerpt-words IF pre-exist
+    while(cutdivs[0]) {
+        cutdivs[0].parentNode.removeChild(cutdivs[0]);
+    }
+
+    // get text from hidden div
+    var textID = document.getElementById('ex-hidden');
+    pagetxt = textID.innerHTML;    
+    //pagetxt = '<p>' + pagetxt + '</p>';
+    //pagetxt = pagetxt.replace(/\r\n\r\n/g, "</p> <p>").replace(/\n\n/g, "</p> <p>")
+    
+// continue until there is nothing in next target to move
+    while  (pagetxt !== "") {
+// repeat while content height of contents of article element is greater than element
+        pagesID = "#z" + pages; 
+        rawID = "z" + pages;
+    
+// check to see if there is a div.excerpt-words#rawID element, make one if not       
+    if ( !document.getElementById(rawID)) {
+         var newDiv = document.createElement("div");
+        newDiv.className = "excerpt-words";
+        newDiv.id = rawID;
+        newDiv.style.maxHeight = pageht + "px";
+         document.getElementById('art-id').appendChild(newDiv);}  
+    
+    var target = document.getElementById(rawID),
+        scrollH = target.scrollHeight;
+    
+// do the actual shifting of text from pagetxt to targetOld
+    while ( !pageDone ) {
+// search for first space in text, cut apart text before and after the space
+        if (pagetxt === "") {break;}
+        textbreak = pagetxt.indexOf("\n\n");
+        textmove = pagetxt.slice(0, textbreak + 2);
+        //textmove = textmove.replace("\n\n", "")
+        textkeep = pagetxt.slice(textbreak + 2);
+
+// write back smaller file to old container
+        pagetxt = textkeep;
+
+// move extra paragraph to new container
+        target.innerHTML += "<p>" + textmove + "</p>";
+        scrollH = target.scrollHeight;
+            while ((scrollH > (pageht - 8))) {
+// 'page' too full, pare back word by word.
+                textover = target.innerHTML;
+                textbreak = textover.lastIndexOf(" ");
+                //find the last word
+                textmove = textover.slice(textbreak);
+                //cut space and word
+                textmove = textmove.replace(/(<([^>]+)>)/ig,"");
+                // removing all tags
+                textmove = textmove.replace(/\.\n\n/g, ".</p><p>");
+                pagetxt = textmove + pagetxt;
+                //put last word back in FRONT of pagetxt (source)
+                textover = textover.slice(0, textbreak);
+                //put remainder from id z(n) in text to keep
+                target.innerHTML = textover;            //replace ALL html with chopped html
+                scrollH = target.scrollHeight;
+                if (scrollH <= pageht - 8) {
+                    //target.innerHTML += "</p>"
+                    pageDone = true;
+                    break;
+                }
+            }
+ 
+        }
+        pageDone = false;
+        createAfterID = pagesID;
+        pages++;
+  }
+}
+       
+// window resize adjust 'page' size, flow text
+$( window ).resize(function() {
+    pageSet($( window ).height());
+});
+
+// document ready adust 'page' size, flow text
+$( document ).ready( function() {
+    pageSet($( window ).height());
+});
